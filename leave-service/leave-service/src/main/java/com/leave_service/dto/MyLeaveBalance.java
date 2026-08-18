1c04223c6726b3dcbeecf9dcfb26e9e43d97d120
karthikmsk <karthikms207@gmail.com>
2026-08-18 20:14:35 +0530

    Added dto to get response for my leave balances

---
+++ leave-service/leave-service/src/main/java/com/leave_service/dto/MyLeaveBalance.java
@@ -0,0 +1,22 @@
+package com.leave_service.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class MyLeaveBalance {
+@NotBlank
+    private Long leaveTypeId;
+
+    @NotBlank
+    private String leaveTypeName;
+
+    private Float remainingDays;
+
+    private Float usedDays;
+
+    @NotNull
+    private Float creditedDays;
+
+}
